c4fb09df5743a9fb98e7c929a649cac6d1ba6e57
Cedric Ziel <cedric@cedric-ziel.com>
2024-08-06 18:00:46 +0200

    fix

--- src/AdvertisingProfilesClient.php
+++ src/AdvertisingProfilesClient.php
@@ -5,6 +5,7 @@ namespace CedricZiel\AmznAdvertisingProfilesApi;
 use CedricZiel\AmznAdvertisingProfilesApi\Generated\Authentication\BearerAuthAuthentication;
 use CedricZiel\AmznAdvertisingProfilesApi\Generated\Client;
 use Http\Client\Common\Plugin\AddHostPlugin;
+use Http\Client\Common\PluginClient;
 use Jane\Component\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
 use Nyholm\Psr7\Uri;
 
