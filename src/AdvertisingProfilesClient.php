f717bd3661a5b9baaa3ce1090d64bd47b63c3b71
Cedric Ziel <cedric@cedric-ziel.com>
2024-08-06 09:44:21 +0200

    fix: use authentication plugin correctly

--- src/AdvertisingProfilesClient.php
+++ src/AdvertisingProfilesClient.php
@@ -5,6 +5,7 @@ namespace CedricZiel\AmznAdvertisingProfilesApi;
 use CedricZiel\AmznAdvertisingProfilesApi\Generated\Authentication\BearerAuthAuthentication;
 use CedricZiel\AmznAdvertisingProfilesApi\Generated\Client;
 use Http\Client\Common\Plugin\AddHostPlugin;
+use Jane\Component\OpenApiRuntime\Client\Plugin\AuthenticationRegistry;
 use Nyholm\Psr7\Uri;
 
 class AdvertisingProfilesClient extends Client
@@ -12,8 +13,10 @@ class AdvertisingProfilesClient extends Client
     public static function createFor(Endpoint $endpoint, string $accessToken, $httpClient = null): AdvertisingProfilesClient
     {
         $plugins = [
+            new AuthenticationRegistry([
+                new BearerAuthAuthentication($accessToken),
+            ]),
             new AddHostPlugin(new Uri($endpoint->value)),
-            new BearerAuthAuthentication($accessToken),
         ];
 
         return parent::create($httpClient, $plugins);
